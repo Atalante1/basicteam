98519614d8991f605b358dd1e5577d429894004c
Asli <aasli4564@gmail.com>
2022-10-09 15:09:58 +0300

    2

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      name: 'Carrie Bradshaw',
+      age: 29,
+      image:
+        'https://i.insider.com/5c95475c77d4f92cad3a2ae2?width=700&format=jpeg&auto=webp',
+    },
+    {
+      id: 2,
+      name: 'Samantha Jones ',
+      age: 32,
+      image:
+        'https://www.marieclaire.com.au/media/100802/and-just-like-that-01.jpg?width=720&center=0.0,0.0',
+    },
+    {
+      id: 3,
+      name: 'Charlotte Goldenblatt',
+      age: 27,
+      image:
+        'https://i.pinimg.com/originals/fa/f8/11/faf81141b298e5e1a4e3ca80b9bee6f2.jpg',
+    },
+    {
+      id: 4,
+      name: 'Miranda Hobbes',
+      age: 34,
+      image:
+        'https://www.tvguide.com/a/img/hub/2019/03/14/a7813514-8a69-443b-836e-e65c90715028/satc-news2.png',
+    },
+    {
+      id: 5,
+      name: 'MBig',
+      age: 29,
+      image:
+        'https://assets.vogue.com/photos/60f6ceca9d2ff0bb3535b048/master/pass/MCDSEAN_EC175.jpg',
+    },
+  ];
